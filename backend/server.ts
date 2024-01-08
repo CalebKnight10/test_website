8487634e177533a937a325c2cf4625557b9f90fa
Caleb Knight <knightca@oregonstate.edu>
2024-01-07 19:23:22 -0700

    Start adding ability to connect to db

--- backend/server.ts
+++ backend/server.ts
@@ -1,24 +1,25 @@
-import express, { Request, Response } from "express";
+import express, { Express, Request, Response } from "express";
 import cors from 'cors';
-
 import morgan from 'morgan';
+import dotenv from "dotenv";
+import connectDB from "./config/db";
 
 const PORT = process.env.PORT || 4000;
+const app: Express = express();
 
-const app = express();
+// Middlewares to accept json in body
 app.use(cors());
-
-// Middleware to accept JSON in body
 app.use(express.json());
 
 // Morgan logging
 app.use(morgan("dev"));
+dotenv.config();
 
-
+connectDB();
+// Routes
 app.get("/", (req: Request, res: Response) => {
   res.send("API IS RUNNING...");
 });
-
 
 app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
